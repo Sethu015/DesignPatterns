65f94fb4d0a6301853229d6a0772f8a96f6a572b
Sethuraman Sundaresan <sethuraman.s@agilysys.com>
2025-06-28 15:00:11 +0530

    Singleton Pattern

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton;
+
+var sing1 = Singleton.Singleton.GetInstance();
+sing1.DisplayMessage("Hello");
+
+var sing2 = Singleton.Singleton.GetInstance();
+sing2.DisplayMessage("Hi");
+
+Console.ReadLine();
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> Instance = new Lazy<Singleton>(() =>  new Singleton());
+        private static int Counter = 0;
+
+        private Singleton()
+        {
+            Counter++;
+            Console.WriteLine($"Counter {Counter}");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return Instance.Value;
+        }
+
+        public void DisplayMessage( string message)
+        {
+            Console.WriteLine( message );
+        }
+    }
+}
